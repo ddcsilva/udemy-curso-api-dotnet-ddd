e417cdf5b5711fd89b330a59f1567fc74c9ac2a1
Danilo Silva <danilo.silva@msn.com>
2022-02-10 22:59:11 -0300

    Criação da Classe de Context e Adição de Referência do Domain

---
+++ Api/Api.Data/Context/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
